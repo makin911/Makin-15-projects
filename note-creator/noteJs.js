c28663d4d3a1425c20076252bd697c429e86a433
Makin <mohamad.alizade991@gmail.com>
2025-11-27 22:52:14 +0330

    note creator project

---
+++ note-creator/noteJs.js
@@ -0,0 +1,45 @@
+
+
+let createButton = document.getElementById('btnCreate');
+//creating each note:
+createButton.addEventListener('click', (element) => {
+  let textArea = document.createElement('textarea');
+  let noteBox = document.getElementById('notes');
+
+  //new div
+  let newDiv = document.createElement('div');
+  newDiv.classList.add('eachBox');
+  noteBox.appendChild(newDiv);
+
+
+  textArea.placeholder = "write here";
+  textArea.classList.add('areaBox');
+  textArea.spellcheck = false;
+  newDiv.appendChild(textArea);
+
+  let delButton = document.createElement('img');
+  delButton.src = "/note-creator/icons8-delete-48.png";
+  delButton.classList.add('del');
+  delButton.onclick = handleDelete;
+  newDiv.appendChild(delButton);
+  saveData();
+})
+
+function handleDelete(e) {
+  e.target.parentElement.remove();
+  saveData();
+
+}
+
+let items = document.getElementById('notes');
+function saveData() {
+  localStorage.setItem('data2', items.innerHTML)
+}
+
+function showTask() {
+  items.innerHTML = localStorage.getItem('data2');
+}
+
+showTask();
+
+
